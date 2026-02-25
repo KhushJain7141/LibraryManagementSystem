33fba26ff39dce0ee2859e476edfb079290f1f94
Khush Vikram Jain <khush.jain@betsol.com>
2026-02-25 16:28:49 +0530

    Updated the auth method,create a route and configured the cors

--- app/main.py
+++ app/main.py
@@ -5,21 +5,30 @@ from app.model import author
 from app.model import book    
 from app.model import category  
 from app.api import authors
+from app.api import auth
 from app.api import books
 from app.api import categories
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
+app = FastAPI( 
     title="Library Management System",
     description="FastAPI + SQLAlchemy layered architecture example",
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.middleware("http")(auth_middleware)
 
 Base.metadata.create_all(bind=engine)
 
-
+app.include_router(auth.router)
 app.include_router(authors.router)
 app.include_router(categories.router)
 app.include_router(books.router)
