2c79cee0f61c91ea7c171c8407e9717c084ee5e0
Khush Vikram Jain <khush.jain@betsol.com>
2026-02-27 11:02:36 +0530

    Made changes to schema to send author books and changes to repo to join author and books table

--- app/repo/author_repo.py
+++ app/repo/author_repo.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session,joinedload
 from app.model.author import Author
 
 
@@ -10,7 +10,12 @@ def create(db: Session, author: Author) -> Author:
 
 
 def get_by_id(db: Session, author_id: int) -> Author | None:
-    return db.query(Author).filter(Author.id == author_id).first()
+    return (
+        db.query(Author)
+        .options(joinedload(Author.books))
+        .filter(Author.id == author_id)
+        .first()
+    )
 
 
 def get_all(db: Session, limit: int | None = None):
